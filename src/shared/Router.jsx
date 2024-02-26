2654d61b3ca6a07dc3dd03ce1083ecb8ee6b0af6
leejieun2023 <lge9380@naver.com>
2024-02-26 20:56:50 +0900

    재수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,11 +7,9 @@ import DetailPage from 'pages/DetailPage';
 import LoginPage from 'pages/LoginPage';
 import SignupPage from 'pages/SignupPage';
 import MyPage from 'pages/MyPage';
-import { useSelector } from 'react-redux';
+import NotFound from 'pages/NotFound';
 
 const Router = () => {
-  const isLogin = useSelector((state) => state.auth.isLogin);
-
   return (
     <BrowserRouter>
       <Routes>
@@ -23,18 +21,17 @@ const Router = () => {
 
         {/* 로그인 상태가 반드시 아니어야 하는 라우터 */}
         <Route element={<NonAuthLayout />}>
-          {!isLogin && (
-            <>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-            </>
-          )}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Route>
 
         {/* 로그인이 필요한 라우터 */}
         <Route element={<AuthLayout />}>
           <Route path="/mypage" element={<MyPage />} />
         </Route>
+
+        {/* 404 Not Found */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
