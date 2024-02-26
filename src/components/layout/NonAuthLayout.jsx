1e1d2334c305fd3b5ab9c0cd4ac03466d757ff1d
leejieun2023 <lge9380@naver.com>
2024-02-26 20:55:07 +0900

    Comment: 주석추가

--- src/components/layout/NonAuthLayout.jsx
+++ src/components/layout/NonAuthLayout.jsx
@@ -1,11 +1,10 @@
 import Navbar from 'components/common/Navbar';
-import useAuth from 'hook/useAuth';
-import { Navigate, Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { Navigate, Outlet } from 'react-router-dom/dist';
 
 const NonAuthLayout = () => {
-  const { isLogin, checked } = useAuth();
-
-  if (checked && isLogin) {
+  const [isLogin, setIsLogin] = useState(false);
+  if (isLogin) {
     alert('이미 로그인 상태입니다.');
     return <Navigate to="/" replace />;
   }
