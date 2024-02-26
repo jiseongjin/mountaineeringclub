deb453797729234bda0a7833e6daf5ad4d6bc869
JIYOUNG <0803jiyoung99@gmail.com>
2024-02-26 21:10:34 +0900

    Feat: 날짜 형식 함수 수정 중

--- src/util/Date.jsx
+++ src/util/Date.jsx
@@ -1,12 +1,29 @@
 export const formattedDate = (timestamp) => {
-  const dateObject = timestamp.toDate();
-  return new Date(dateObject).toLocaleDateString('ko-KR', {
-    year: '2-digit',
-    month: '2-digit',
-    day: '2-digit',
-    weekday: 'short',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  });
+  if (timestamp instanceof Date) {
+    // 이미 Date 객체인 경우 변환 필요 없음
+    return new Date(timestamp).toLocaleDateString('ko-KR', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      weekday: 'short',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+  } else if (timestamp.toDate instanceof Function) {
+    // Firestore Timestamp 객체인 경우 toDate() 메서드 사용
+    return timestamp.toDate().toLocaleDateString('ko-KR', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      weekday: 'short',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+  } else {
+    // 그 외의 경우 예외 처리 또는 기본값 반환
+    console.error('Invalid timestamp:', timestamp);
+    return '';
+  }
 };
