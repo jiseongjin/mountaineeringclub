6a3f37d43ed7309480d8e63adf6c21a6d5101c0d
Your Name <you@your-domain.com>
2024-02-26 10:36:01 +0900

    Feat:지도에 검색한 장소확인 기능구현

--- src/components/detail/KakaoMap.jsx
+++ src/components/detail/KakaoMap.jsx
@@ -3,13 +3,41 @@ import styled from 'styled-components';
 
 const KakaoMap = () => {
   useEffect(() => {
+    var infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
+
     var container = document.getElementById('map');
     var options = {
       center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
       level: 3
     };
     var map = new window.kakao.maps.Map(container, options);
+    var ps = new window.kakao.maps.services.Places();
+    ps.keywordSearch('북한산 등산코스', placesSearchCB);
+
+    function placesSearchCB(data, status, pagination) {
+      if (status === window.kakao.maps.services.Status.OK) {
+        var bounds = new window.kakao.maps.LatLngBounds();
+
+        for (var i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+          bounds.extend(new window.kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+
+        map.setBounds(bounds);
+      }
+    }
+    function displayMarker(place) {
+      var marker = new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(place.y, place.x)
+      });
+      window.kakao.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+      });
+    }
   }, []);
+
   return <ImgBox id="map"></ImgBox>;
 };
 
