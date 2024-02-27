71c4f53b1784f6067910cb3a8be0671ad54655d2
leejieun2023 <lge9380@naver.com>
2024-02-27 21:41:29 +0900

    머지했는데 수정파일 있었음

--- src/components/detail/HikingTrail.jsx
+++ src/components/detail/HikingTrail.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-
-const HikingTrail = () => {
-  const [data, SetData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios({
-          url: 'https://api.vworld.kr/req/data',
-          method: 'GET',
-          params: {
-            key: 'C52BF50F-4E62-3A15-B415-2D05A786EA03',
-            attrFilter: 'mntn_nm:=:삼성산|emdCd:=:41171102',
-            data: 'LT_L_FRSTCLIMB',
-            request: 'GetFeature',
-            size: 1000
-          }
-        });
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
-  return (
-    <HikingTrailInformationBox>
-      <InformationBox>
-        <p>등산 코스</p>
-        <ImgBox id="map"></ImgBox>
-        <CourseInformationBox>
-          <p>난이도 : </p>
-          <p>소요시간 : </p>
-          <p>코스길이 : </p>
-          <p>고도 : </p>
-        </CourseInformationBox>
-      </InformationBox>
-    </HikingTrailInformationBox>
-=======
 import styled from 'styled-components';
 import KakaoMap from './KakaoMap';
 import mountainData from 'mountainData.json';
@@ -76,7 +33,6 @@ const HikingTrail = () => {
         </InformationBox>
       </HikingTrailInformationBox>
     </>
->>>>>>> dev
   );
 };
 
@@ -97,21 +53,6 @@ const InformationBox = styled.article`
   flex-direction: column;
 `;
 
-<<<<<<< HEAD
-const ImgBox = styled.div`
-  margin-top: 10px;
-  background-color: green;
-  /* background-image: url(''); */
-  height: 400px;
-  width: 500px;
-`;
-
-const CourseInformationBox = styled.div`
-  width: 1000px;
-  display: flex;
-  justify-content: space-between;
-  margin-top: 10px;
-=======
 const CourseInformationBox = styled.div`
   width: 1200px;
   display: flex;
@@ -134,5 +75,4 @@ const MntiDetail = styled.h5`
 const LowBox = styled.div`
   display: flex;
   gap: 10px;
->>>>>>> dev
 `;
