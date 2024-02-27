67ceb0bccdb851979478570447bcca3d072a2b6b
Your Name <you@your-domain.com>
2024-02-27 16:39:23 +0900

    Refactor:리셋

--- src/components/detail/HikingTrail.jsx
+++ src/components/detail/HikingTrail.jsx
@@ -1,48 +1,26 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import KakaoMap from './KakaoMap';
-import { isArray } from 'lodash';
+import mountainData from '';
 
 const HikingTrail = () => {
   // useParams 이용하기
   const params = '북한산';
   // 산 데이터
-  const [mountainData, setMountainData] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(
-          'https://apis.data.go.kr/1400000/service/cultureInfoService2/mntInfoOpenAPI2',
-          {
-            params: {
-              ServiceKey: '0wHFN3EE7v+jLjujPukh2tGtJj/yCRpvhr5reMlXtjDkWobuC62OIZ+c9fLJ3VbRN3ocF9r3hWOj3r/LaWtf3w==',
-              searchWrd: params
-            }
-          }
-        );
-        const mountainDatas = data.response.body.items.item;
-        console.log(mountainDatas);
-        isArray(mountainDatas)
-          ? setMountainData(...mountainDatas.filter((item) => item.mntihigh > 100))
-          : setMountainData(mountainDatas);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-    fetchData();
-  }, []);
+  const mountainDb = mountainData;
+
+  console.log(mountainDb);
+
   return (
     <HikingTrailInformationBox>
       <InformationBox>
         <KakaoMap params={params} />
         {/* <ImgBox /> */}
-        <MntinName>{params}</MntinName>
+        <MntinName>산이름</MntinName>
         <CourseInformationBox>
-          <MntiDetail>{mountainData?.mntidetails}</MntiDetail>
+          <MntiDetail>내용</MntiDetail>
           {/* <p>소요시간 : </p>
           <p>코스길이 : </p> */}
-          <p>높이 : {mountainData?.mntihigh}M</p>
+          <p>높이 : ?? M</p>
         </CourseInformationBox>
       </InformationBox>
     </HikingTrailInformationBox>
@@ -72,12 +50,6 @@ const CourseInformationBox = styled.div`
   flex-direction: column;
   margin-top: 10px;
   gap: 10px;
-`;
-
-const ImgBox = styled.img`
-  margin-top: 10px;
-  height: 400px;
-  width: 1000px;
 `;
 
 const MntinName = styled.h2`
