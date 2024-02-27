01930b0bcfc27beb42827c5f0a7de6f6efeeaa81
limhyerin <rin091345@gmail.com>
2024-02-27 17:16:14 +0900

    Design: 디자인 css 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,15 +3,12 @@ import { auth, db, storage } from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import styled from 'styled-components';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { signOut } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
 import { doc, getDoc, updateDoc, setDoc } from "firebase/firestore";
 
 const MyPage = () => {
   const [imageUrl, setImageUrl] = useState("");
   const [nickName, setNickname] = useState("");
   const [newNickName, setNewNickname] = useState("");
-  const navigate = useNavigate();
   const [activeButton, setActiveButton] = useState(); 
 
   useEffect(() => {
@@ -34,7 +31,7 @@ const MyPage = () => {
     };
   }, []);
   
-
+  // 프로필 이미지 업로드
   const handleProfileImageUpload = async (event) => {
     const file = event.target.files[0];
     const storageRef = ref(storage, `profileImages/${file.name}`);
@@ -59,27 +56,15 @@ const MyPage = () => {
     });
   };
   
-  // 닉네임 업데이트 하는 함수
+  // 닉네임 업데이트
   const handleNicknameChange = async () => {
     const user = auth.currentUser;
     const userDoc = doc(db, 'users', user.uid);
     try {
-      // 닉네임 업데이트
       await updateDoc(userDoc, { nickName: newNickName });
       setNickname(newNickName);
       setNewNickname("");
       alert('닉네임이 변경되었습니다.');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  // 로그아웃 후 로그인 페이지 이동
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      alert('로그아웃 되었습니다.');
-      navigate('/login');
     } catch (error) {
       console.error(error);
     }
@@ -91,28 +76,32 @@ const MyPage = () => {
           <StProfile src={imageUrl} alt="프로필 사진" />
           <StprofileNickName><strong>{nickName}</strong>님</StprofileNickName>
           <StButtons>
-            <StBtn active={activeButton === '내 정보 수정'} onClick={() => setActiveButton('내 정보 수정')}>내 정보 수정</StBtn>
-            <StBtn active={activeButton === '스크랩'} onClick={() => setActiveButton('스크랩')}>스크랩</StBtn>
-            <StBtn active={activeButton === '완주한 등산코스'} onClick={() => setActiveButton('완주한 등산코스')}>완주한 등산코스</StBtn>
-            <StBtn active={activeButton === '작성한 댓글'} onClick={() => setActiveButton('작성한 댓글')}>작성한 댓글</StBtn>
-            <StLogoutBtn onClick={handleLogout}>로그아웃</StLogoutBtn>
+            <StBtn active={activeButton === '내 정보 수정'} onClick={() => setActiveButton('내 정보 수정')}><span>내 정보 수정</span></StBtn>
+            <StBtn active={activeButton === '스크랩'} onClick={() => setActiveButton('스크랩')}><span>스크랩</span></StBtn>
+            <StBtn active={activeButton === '완주한 등산코스'} onClick={() => setActiveButton('완주한 등산코스')}><span>완주한 등산코스</span></StBtn>
+            <StBtn active={activeButton === '작성한 댓글'} onClick={() => setActiveButton('작성한 댓글')}><span>작성한 댓글</span></StBtn>
           </StButtons>
         </StMenu>
         <StContent>
           {activeButton === '내 정보 수정' ? (
-            <div>
-              <StTitle>프로필 수정</StTitle>
-              <StImage src={imageUrl} alt="프로필 사진" />
-              <div>
-                <StFileInput type="file" id="file" onChange={handleProfileImageUpload} style={{display: 'none'}} />
-                <StFileInputLabel htmlFor="file">파일 선택</StFileInputLabel>
-              </div>
-              <StEditNickName>{nickName}</StEditNickName>
-              <div>
-                <StInputNickName type="text" value={newNickName} onChange={(e) => setNewNickname(e.target.value)} />
-                <StInputButton onClick={handleNicknameChange}>닉네임 변경</StInputButton>
-              </div>
-            </div>
+            <>
+              <StTitle>내 정보 수정</StTitle>
+              <StEditBox>
+                <StProfileBox>
+                  <StImage src={imageUrl} alt="프로필 사진" />
+                </StProfileBox>
+                <StProfileNickNameEditBox>
+                  <StContext>프로필 이미지 변경</StContext>
+                  <StFileInput type="file" id="file" onChange={handleProfileImageUpload} style={{display: 'none'}} />
+                  <StFileInputLabel htmlFor="file">파일 선택</StFileInputLabel>
+
+                  <StContext>닉네임 변경</StContext>
+                  <StEditNickName>{nickName}</StEditNickName>
+                  <StInputNickName type="text" value={newNickName} onChange={(e) => setNewNickname(e.target.value)} placeholder='변경할 닉네임을 입력해주세요'/>
+                  <StInputButton onClick={handleNicknameChange}>변경</StInputButton>
+                </StProfileNickNameEditBox>
+              </StEditBox>
+            </>
           ) : activeButton === '스크랩' ? (
             <div>
               {/* 스크랩 목록 */}
@@ -127,6 +116,7 @@ const MyPage = () => {
             </div>
           ) : (
             <StProfilebox>
+              <StTitle>마이페이지</StTitle>
               <StImage src={imageUrl} alt="프로필 사진" />
               <StNickName>{nickName}</StNickName>
             </StProfilebox>
@@ -138,6 +128,9 @@ const MyPage = () => {
 
 const StContainer = styled.div`
   display: flex;
+  height: 500px;
+  margin-top: 30px;
+  background-color: white;
 `;
 
 const StProfilebox = styled.div`
@@ -154,7 +147,6 @@ const StImage = styled.img`
 
 const StNickName = styled.p`
   font-size: 25px;
-  font-weight: bold;
 `;
 
 // Menu
@@ -163,9 +155,8 @@ const StMenu = styled.div`
   height: 500px;
   text-align: center;
   justify-content: center;
-  border-radius: 10px;
-  border: solid 2px;
-  border-color:var(--sub-color2);
+  margin-left: 20px;
+  //background-color: var(--sub-color2);
 `;
 
 // Menu : 프로필 이미지
@@ -196,35 +187,41 @@ const StBtn = styled.button`
   width: 130px;
   height: 40px;
   margin: 0px auto;
-  background-color: ${props => props.active ? 'var(--sub-color3)' : 'transparent'}; 
+  background-color: transparent; 
   border: none;
   position: relative;
   border-radius: 5px; 
-  &:hover {
-    background-color: var(--sub-color2); 
+  
+  & span {
+    position: relative;
+    &:after {
+      content: '';
+      position: absolute;
+      bottom: -5px;
+      left: 50%;
+      width: ${props => props.active ? '100%' : '0'};
+      transform: ${props => props.active ? 'translateX(-50%)' : 'translateX(0)'};
+      border-bottom: 3px solid ${props => props.active ? 'var(--sub-color1)' : 'var(--sub-color2)'};
+      transition: transform 0.3s ease-in-out, width 0.3s ease-in-out;
+    }
   }
-  transition: background-color 0.1s;
-`;
 
-// Menu : 로그아웃 버튼
-const StLogoutBtn = styled.button`
-  width: 130px;
-  height: 40px;
-  margin: 40px auto;
-  border-radius: 10px;
-  border-color: gray;
+  &:hover span::after {
+    width: 100%;
+    transform: translateX(-50%);
+  }
 `;
 
 // content
 const StContent = styled.div`
   width: 80%;
-  // background-color: var(--sub-color3);
+  background-color: #ffffff9d;
   text-align: center;
-  margin: 0px 50px 0px 50px;
-  
+  margin: 20px 50px 0px 50px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19) ;
   border-radius: 10px;
   border: solid 2px;
-  border-color: var(--sub-color2);
+  border-color: transparent;
 `;
 
 // content-내 정보 수정 : 타이틀
@@ -232,20 +229,43 @@ const StTitle = styled.p`
   font-size: 30px;
   margin-top: 40px;
   font-weight: bold;
+  color: #476442;
+`;
+
+const StEditBox = styled.div`
+  display: flex;
+`;
+
+const StProfileNickNameEditBox = styled.div`
+  margin: auto auto;
+`;
+
+// content- 내 정보 수정 : profile 이미지
+const StProfileBox = styled.div`
+  margin: 50px auto;
+`;
+
+const StContext = styled.p`
+  color: #5f4d3e;
+  font-size: 20px;
+  font-weight: bold;
 `;
 
 // content-내 정보 수정 : 파일 선택
 const StFileInputLabel = styled.label`
   display: inline-block;
   padding: 0.5em 1.5em;
-  color: #fff;
-  background-color: #949494;
-  border-radius: 4px;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  color: #000000;
+  background-color: var(--sub-color3);
+  border-color: #d5e6d26c;
+  border-radius: 10px;
   text-align: center;
   transition: background-color 0.2s ease;
   cursor: pointer;
   &:hover {
-    background-color: #7e7e7e;
+    background-color: #b8c9b56c;
   }
 `;
 const StFileInput = styled.input`
@@ -262,8 +282,10 @@ const StEditNickName = styled.p`
 // content-내 정보 수정 : 닉네임 변경 input 창
 const StInputNickName = styled.input`
   width: 200px;
-  height: 30px;
-
+  height: 40px;
+  border-color: #ffffff6d;
+  border-radius: 10px;
+  background-color: #e2e2e26e;
 `;
 
 // content-내 정보 수정 : 닉네임 변경 버튼
@@ -272,10 +294,10 @@ const StInputButton = styled.button`
   height: 40px;
   margin-left: 5px;
   border-radius: 10px;
-  border-color: transparent;
-  background-color: var(--sub-color2);
+  border-color: #d5e6d26c;
+  background-color: var(--sub-color3);
   &:hover {
-    background-color: var(--sub-color1);
+    background-color: #b8c9b56c;
   }
 `;
 
