7ace864f3f6c0b96c46386e86f8ac9b03cbacdff
limhyerin <rin091345@gmail.com>
2024-02-29 10:48:55 +0900

    Refector:merge

--- src/firebase.js
+++ src/firebase.js
@@ -10,11 +10,7 @@ import { getStorage } from 'firebase/storage';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_API_KEY,
-<<<<<<< HEAD
   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-=======
-  authDomain: process.env.RREACT_APP_FB_AUTH_DOMAIN,
->>>>>>> b60fdcde655665acb9f90a31721d1be305b8fcbb
   projectId: process.env.REACT_APP_FB_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
